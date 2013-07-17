af3b7c60cc23b7cf804da448ef7233d9767d5636
Orry Baram <orrybaram@gmail.com>
2013-07-17 00:06:57 -0400

    got rid of shit

--- js/main.js
+++ js/main.js
@@ -156,17 +156,6 @@ function init() {
 
 	window.addEventListener( 'resize', onWindowResize, false );
 
-	// Postprocessing
-	// =============================================
-
-	var renderModel = new THREE.RenderPass( scene, camera );
-	var effectFilm = new THREE.FilmPass( 0.35, 0.75, 2048, false );
-	effectFilm.renderToScreen = true;
-
-	composer = new THREE.EffectComposer( renderer );
-
-	composer.addPass( renderModel );
-	composer.addPass( effectFilm );
 
 };
 
